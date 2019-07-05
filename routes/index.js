d61707731664659e2bbe5c328c2044ef10756aeb
Stefano Vanotti <vanottistefano@gmail.com>
2019-07-05 12:18:16 +0200

    commit pre-lunch friday

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,31 @@ router.get("/rentbike", (req, res) => {
     });
 });
 
+router.post("/rentbike", (req, res) => {
+  if (req.body.bodytype != "") {
+    console.log(req.body.bodytype);
+    Product.find({ bodytype: req.body.bodytype })
+      .then(product => {
+        // product.forEach(element => {});
+        console.log(product);
+        res.render("products", { product, scripts: ["rentbikes.js"] });
+      })
+      .catch(err => {
+        res.render("products", { err });
+      });
+  } else {
+    Product.find({})
+      .then(product => {
+        product.forEach(element => {});
+        console.log(product);
+        res.render("products", { product, scripts: ["rentbikes.js"] });
+      })
+      .catch(err => {
+        res.render("products", { err });
+      });
+  }
+});
+
 router.get("/rentbike/:id", (req, res) => {
   Product.findById(req.params.id)
     .then(product => {
